47735102cc7ffe9ec998d2612982c100586e29c4
Zaiste <oh@zaiste.net>
2023-05-12 15:14:33 +0200

    Fix `domains` + update SDK

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const instanceURL = new URL(process.env.NEXT_PUBLIC_SALEOR_INSTANCE_URI);
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [instanceURL.hostname],
+    domains: [instanceURL.hostname, "prod.demo.saleor.cloud"],
   },
 };
 
